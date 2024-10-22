e8d763c02151cf72cbd65a97c19ef26f4ba195dd
Haru-Tachibana <paloma030415@gmail.com>
2024-10-22 16:02:26 +0100

    Create EDA.py

---
+++ Practical/Week 5 MAGICC climate modelling/EDA.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+SSP5_85 = pd.read_csv('/Users/yangyangxiayule/Documents/GitHub/Environmental-Modelling/Practical/Week 5 MAGICC climate modelling/Practical/TEMPLATES/SSP5-8.5_templ')
